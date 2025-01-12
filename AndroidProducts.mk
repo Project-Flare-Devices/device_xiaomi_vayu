050655ed0ed5905761429ea0d5a9af5f0ebff15e
GXC2356 <gxcdev2356@gmail.com>
2025-01-12 18:34:12 +0200

    Flare bringup!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_vayu.mk
+    $(LOCAL_DIR)/flare_vayu.mk
--- flare_vayu.mk
+++ flare_vayu.mk
@@ -11,21 +11,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common Aosp configurations
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit common Flare configurations
+$(call inherit-product, vendor/flare/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_AOSP_RECOVERY := true
-TARGET_DISABLE_EPPE := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
-# SkylineUI Maintainer Flags
-SKYLINEUI_MAINTAINER := GXC2356
-CUSTOM_BUILD_TYPE := OFFICIAL
+# ProjectFlare flags
+FLARE_BUILD_TYPE := OFFICIAL
+FLARE_MAINTAINER := GXC2356
+WITH_GAPPS := true
 
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
-PRODUCT_NAME := aosp_vayu
+PRODUCT_NAME := flare_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
